b1334c354ec5191dcc54a0ed8113db0799006365
wajdturki <47699315+wajdturki@users.noreply.github.com>
2021-05-24 15:10:27 +0300

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,10 @@
 # 1 
-for i in range(1, 100):
+for i in range(1, 100):# Numbers between 1 and 100
     print(i)
+
+    
+# 2
+for i in range(1,100): # Numbers between 1 and 100
+    if i % 8 == 0:
+# If i is divisible by 8 then ptint it.
+        print(i)
