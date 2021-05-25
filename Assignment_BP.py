9ea7ce34ba21d6ac57ad21a9670b6963bad76f75
wajdturki <47699315+wajdturki@users.noreply.github.com>
2021-05-25 16:01:15 +0300

    Create Assignment_BP.py

---
+++ Assignment_BP.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy import stats
+import math
+
+def prob(y):
+    print("The number of people with a probability of:",y)
+
+    #sample size:-
+    birthdays=365
+
+    #trials
+    z=5
+    for i in range(z):
+        #print(i)
+        results = np.array([])
+        for people in range(1,25):
+             #return a random num from 1to 365 representing a birth date
+            birthdate=np.random.randint(1,birthdays+1,people)
+
+            if len(birthdate) == len (set(birthdate)):
+                     results = np.append(results,0)
+            else:
+                results = np.append(results,1)
+            x=sum(results)      
+            probablity=x/z
+            if (probablity > y):
+                print("The probablity of ",people," people have same birthday is ",probablity)
+            #print(x)
+prob(0.5)
+prob(0.95)
+prob(0.99)
